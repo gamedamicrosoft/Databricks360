7ae76d3235f59d28006035f410faed17dfc9465f
gamedamicrosoft <gameda@microsoft.com>
2024-10-05 09:57:35 -0400

    updating config and and other bash script

--- iac-adb-360/helpers/rg-create.sh
+++ iac-adb-360/helpers/rg-create.sh
@@ -3,7 +3,7 @@
 solutionname='adb360'
 location='westus3'
 locationshortname='wus3'
-subscriptionid='7db7a1c4-bd16-4835-beab-1ccefcb72d90'
+subscriptionid='1bf6f6d4-b209-4338-abc0-702ab91dd764'
 serviceprincipalname='devops-sc'
 adbinteractprincipalname='adb360-sp'
 locationshortname='wus3'
@@ -18,10 +18,10 @@ rgDev="rg-$locationshortname-$dailysolutionname-dev"
 rgPrd="rg-$locationshortname-$dailysolutionname-prd"
 
 # get service principal object id
-serviceprincipaloid=$(az ad sp list --display-name $serviceprincipalname --query "[].id" -o tsv)
+serviceprincipaloid=$(az ad sp list --display-name $serviceprincipalname --query "[].appId" -o tsv)
 echo "found $serviceprincipaloid for devops-sc"
 
-adbspoid=$(az ad sp list --display-name $adbinteractprincipalname --query "[].id" -o tsv)
+adbspoid=$(az ad sp list --display-name $adbinteractprincipalname --query "[].appId" -o tsv)
 echo "found $adbspoid for adb-sp"
 
 erg=$(az group list --query "[?name=='$rgDev'].name" -o tsv)
@@ -29,16 +29,16 @@ if [ -z "$erg" ]
     then 
         echo 'resourcegroup does not exist, create it'
         az group create -n $rgDev -l $location
-        az role assignment create --role 'Contributor' --assignee $serviceprincipaloid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgDev"
-        az role assignment create --role 'Contributor' --assignee $adbspoid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgDev"
-        az role assignment create --role 'User Access Administrator' --assignee $serviceprincipaloid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgDev"
+        az role assignment create --role 'Contributor' --assignee $serviceprincipaloid --scope "subscriptions/$subscriptionid/resourceGroups/$rgDev"
+        az role assignment create --role 'Contributor' --assignee $adbspoid --scope "subscriptions/$subscriptionid/resourceGroups/$rgDev"
+        az role assignment create --role 'User Access Administrator' --assignee $serviceprincipaloid --scope "subscriptions/$subscriptionid/resourceGroups/$rgDev"
     else
         echo 'resourcegroup exists, delete and recreate it'
         az group delete -n $rgDev -y
         az group create -n $rgDev -l $location
-        az role assignment create --role 'Contributor' --assignee $serviceprincipaloid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgDev"
-        az role assignment create --role 'Contributor' --assignee $adbspoid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgDev"
-        az role assignment create --role 'User Access Administrator' --assignee $serviceprincipaloid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgDev"
+        az role assignment create --role 'Contributor' --assignee $serviceprincipaloid --scope "subscriptions/$subscriptionid/resourceGroups/$rgDev"
+        az role assignment create --role 'Contributor' --assignee $adbspoid --scope "subscriptions/$subscriptionid/resourceGroups/$rgDev"
+        az role assignment create --role 'User Access Administrator' --assignee $serviceprincipaloid --scope "subscriptions/$subscriptionid/resourceGroups/$rgDev"
 fi
 
 erg=$(az group list --query "[?name=='$rgPrd'].name" -o tsv)
@@ -46,16 +46,16 @@ if [ -z "$erg" ]
     then 
         echo 'resourcegroup does not exist, create it'
         az group create -n $rgPrd -l $location
-        az role assignment create --role 'Contributor' --assignee $serviceprincipaloid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgPrd"
-        az role assignment create --role 'Contributor' --assignee $adbspoid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgPrd"
-        az role assignment create --role 'User Access Administrator' --assignee $serviceprincipaloid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgPrd"
+        az role assignment create --role 'Contributor' --assignee $serviceprincipaloid --scope "subscriptions/$subscriptionid/resourceGroups/$rgPrd"
+        az role assignment create --role 'Contributor' --assignee $adbspoid --scope "subscriptions/$subscriptionid/resourceGroups/$rgPrd"
+        az role assignment create --role 'User Access Administrator' --assignee $serviceprincipaloid --scope "subscriptions/$subscriptionid/resourceGroups/$rgPrd"
     else
         echo 'resourcegroup exists, delete and recreate it'
         az group delete -n $rgPrd -y
         az group create -n $rgPrd -l $location
-        az role assignment create --role 'Contributor' --assignee $serviceprincipaloid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgPrd"
-        az role assignment create --role 'Contributor' --assignee $adbspoid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgPrd"
-        az role assignment create --role 'User Access Administrator' --assignee $serviceprincipaloid --scope "/subscriptions/$subscriptionid/resourceGroups/$rgPrd"
+        az role assignment create --role 'Contributor' --assignee $serviceprincipaloid --scope "subscriptions/$subscriptionid/resourceGroups/$rgPrd"
+        az role assignment create --role 'Contributor' --assignee $adbspoid --scope "subscriptions/$subscriptionid/resourceGroups/$rgPrd"
+        az role assignment create --role 'User Access Administrator' --assignee $serviceprincipaloid --scope "subscriptions/$subscriptionid/resourceGroups/$rgPrd"
 fi
 
 echo 'finished'
--- iac-adb-360/helpers/sc-create.sh
+++ iac-adb-360/helpers/sc-create.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-adoproject='adb360-0614'
+adoproject='adb360-051024'
 
 export AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY=''
 # azure rm ado-sc
-az devops service-endpoint azurerm create --azure-rm-service-principal-id 2732c10d-18e1-4749-a855-3b47daf5dfe1 --azure-rm-subscription-id 3d11a8f9-16c2-438e-bbed-3b23505340ec --azure-rm-subscription-name ME-MngEnv289593-chrey --azure-rm-tenant-id 12ce7121-18c7-4841-98f9-3b26fc8af34f --name ado-sc --org https://dev.azure.com/hdikram --project $adoproject
+az devops service-endpoint azurerm create --azure-rm-service-principal-id 3afd1a53-fbef-4156-bbf3-13198770cd07 --azure-rm-subscription-id 1bf6f6d4-b209-4338-abc0-702ab91dd764 --azure-rm-subscription-name ME-MngEnvMCAP211549-gameda-1 --azure-rm-tenant-id 6305e991-1d65-4066-8d87-77c2111eb124 --name ado-sc --org https://dev.azure.com/medagan-admin --project $adoproject
 
 export AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY=''
 # azure rm ado-sp
-az devops service-endpoint azurerm create --azure-rm-service-principal-id ee429c67-2078-47b5-8bcb-b4c66b02bb24 --azure-rm-subscription-id 3d11a8f9-16c2-438e-bbed-3b23505340ec --azure-rm-subscription-name ME-MngEnv289593-chrey --azure-rm-tenant-id 12ce7121-18c7-4841-98f9-3b26fc8af34f --name adb-sc --org https://dev.azure.com/hdikram --project $adoproject
+az devops service-endpoint azurerm create --azure-rm-service-principal-id c8eeb609-fc1f-416c-90db-19909bb1a677 --azure-rm-subscription-id 1bf6f6d4-b209-4338-abc0-702ab91dd764 --azure-rm-subscription-name ME-MngEnvMCAP211549-gameda-1 --azure-rm-tenant-id 6305e991-1d65-4066-8d87-77c2111eb124 --name adb-sc --org https://dev.azure.com/medagan-admin --project $adoproject
 
 
 # github
 export AZURE_DEVOPS_EXT_GITHUB_PAT=''
-az devops service-endpoint github create --github-url https://github.com/chrey-gh/Databricks360.git --name gh-sc --org https://dev.azure.com/hdikram --project $adoproject
+az devops service-endpoint github create --github-url https://github.com/gamedamicrosoft/Databricks360.git --name gh-sc --org https://dev.azure.com/medagan-admin --project $adoproject
 
